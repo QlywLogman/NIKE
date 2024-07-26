c814612f238e8641949ea05d4be95e67bad08071
Quliyev Logman Hikmet <Quliy_nx74@itstep.edu.az>
2024-07-26 14:37:00 +0400

    NIKE

---
+++ NIKE/Views/AdminPages/Shope_pageView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NIKE.Views.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for Shope_pageView.xaml
+    /// </summary>
+    public partial class Shope_pageView : Page
+    {
+        public Shope_pageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
